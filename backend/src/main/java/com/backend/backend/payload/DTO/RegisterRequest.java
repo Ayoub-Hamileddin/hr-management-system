b5c526a21b92fd18cbab412fffc2f1cbecd11b1e
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-15 00:34:34 +0100

    Update RegisterRequest DTO: adjust fields for new register logic

--- backend/src/main/java/com/backend/backend/payload/DTO/RegisterRequest.java
+++ backend/src/main/java/com/backend/backend/payload/DTO/RegisterRequest.java
@@ -1,6 +1,5 @@
 package com.backend.backend.payload.DTO;
 
-import com.backend.backend.domain.Role;
 
 import lombok.Builder;
 import lombok.Data;
@@ -10,13 +9,12 @@ import lombok.Data;
 @Data
 @Builder
 public class RegisterRequest {
-    private String fullName;
+    private String firstName;
+
+    private String lastName;
    
     private String email;
     
     private String password;
 
-    private Role role;
-
-    private Boolean isActive;
 }
