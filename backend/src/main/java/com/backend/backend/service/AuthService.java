d15d9ae175790d64113594e7a18ede31ec1fb2b2
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-16 03:47:32 +0100

    Update AuthService: add refreshToken method signature

--- backend/src/main/java/com/backend/backend/service/AuthService.java
+++ backend/src/main/java/com/backend/backend/service/AuthService.java
@@ -1,12 +1,17 @@
 package com.backend.backend.service;
 
 import com.backend.backend.payload.DTO.LoginRequest;
+import com.backend.backend.payload.DTO.RefreshRequest;
 import com.backend.backend.payload.DTO.RegisterRequest;
 import com.backend.backend.payload.DTO.UserDto;
 import com.backend.backend.payload.response.AuthResponse;
 
 public interface AuthService {
     AuthResponse login(LoginRequest loginRequest);
+
     AuthResponse register(RegisterRequest registerRequest);
+
     UserDto me(String jwt);
+
+    String logout(RefreshRequest refreshRequest);
 }
