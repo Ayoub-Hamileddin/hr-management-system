f7180625955a9dd79e6b9f70087a884c81a57a12
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-16 03:47:32 +0100

    Update UserMapper: map refresh response if needed

--- backend/src/main/java/com/backend/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/backend/backend/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import com.backend.backend.payload.DTO.UserDto;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserMapper INSTANCE=Mappers.getMapper(UserMapper.class);
+    // UserMapper INSTANCE=Mappers.getMapper(UserMapper.class);
 
     UserDto toDto(User user);
     User toEntity(UserDto userDto);
