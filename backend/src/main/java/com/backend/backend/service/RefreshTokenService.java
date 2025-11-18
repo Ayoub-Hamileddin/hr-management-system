a74b2fc39c1ae4defd17efad7419c9358deed534
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-18 01:14:10 +0100

    Update RefreshTokenService interface: create & verify refresh tokens

--- backend/src/main/java/com/backend/backend/service/RefreshTokenService.java
+++ backend/src/main/java/com/backend/backend/service/RefreshTokenService.java
@@ -2,12 +2,15 @@ package com.backend.backend.service;
 
 import com.backend.backend.model.RefreshToken;
 
+
 public interface RefreshTokenService {
     
     RefreshToken createRefreshToken(Long userId,String refreshToken);
 
-    Boolean revoke(RefreshToken token);
+     void revokeToken(RefreshToken token);
 
     Boolean isExpired(RefreshToken token);
+
+   
     
 }
