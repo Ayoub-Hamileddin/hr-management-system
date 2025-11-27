b3a1158217f21d0ad0689a3307ada50046a799cb
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-27 02:51:04 +0100

    Fix UserMapper mappings and DTO conversions

--- backend/src/main/java/com/backend/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/backend/backend/mapper/UserMapper.java
@@ -1,14 +1,12 @@
 package com.backend.backend.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import com.backend.backend.model.User;
 import com.backend.backend.payload.DTO.UserDto;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    // UserMapper INSTANCE=Mappers.getMapper(UserMapper.class);
 
     UserDto toDto(User user);
     User toEntity(UserDto userDto);
