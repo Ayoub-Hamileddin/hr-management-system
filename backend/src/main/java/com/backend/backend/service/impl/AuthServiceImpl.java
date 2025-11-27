2fbc6f207bddd6a209939b0ed731db3d4983bbf7
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-27 02:51:17 +0100

    Implement authentication logic updates in AuthServiceImpl

--- backend/src/main/java/com/backend/backend/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/backend/backend/service/impl/AuthServiceImpl.java
@@ -2,8 +2,13 @@ package com.backend.backend.service.impl;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +28,7 @@ import com.backend.backend.repository.UserRepository;
 import com.backend.backend.service.AuthService;
 import com.backend.backend.service.RefreshTokenService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -37,10 +43,11 @@ public class AuthServiceImpl implements AuthService {
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
-
+    @Value("${jwt.refresh-token-expiration}")
+    private Long refresh_token_expiration;
                 //* login logic   */
     @Override
-    public AuthResponse login(LoginRequest loginRequest) {
+    public AuthResponse login(LoginRequest loginRequest,HttpServletResponse response) {
         authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(
                 loginRequest.getEmail(),
@@ -57,11 +64,22 @@ public class AuthServiceImpl implements AuthService {
                      //* create and save the refresh_token  */
         refreshTokenService.createRefreshToken(user.getId(), refresh_token);
 
+
+        ResponseCookie newRefreshToken=ResponseCookie.from("refreshToken",refresh_token)
+        .httpOnly(true)
+        .secure(true)
+        .path("/")
+        .sameSite("None")
+        .maxAge(refresh_token_expiration)
+        .build();
+
+        response.addHeader("Set-Cookie", newRefreshToken.toString());
+
         return AuthResponse.builder()
             .access_token(access_token)
             .refresh_token(refresh_token)
             .message("Login Successfuly")
-            .userDto(userMapper.toDto(user))
+            .user(userMapper.toDto(user))
 
         .build();
 
@@ -98,7 +116,6 @@ public class AuthServiceImpl implements AuthService {
             .access_token(access_token)
             .refresh_token(refresh_token)
             .message("Register Successfuly")
-           
 
        .build();
 
@@ -119,16 +136,12 @@ public class AuthServiceImpl implements AuthService {
 
                     //* get current user info   */
     @Override
-    public UserDto me(String jwt) {
+    public UserDto me() {
 
-       String userEmail=jwtService.extractUserName(jwt);
-
-        User connectedUser=userRepository.findByEmail(userEmail).orElseThrow(
-            ()->new UsernameNotFoundException("Current User Not Found")
-        );
-
-
-        return userMapper.toDto(connectedUser);
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final UserDetails userDetails = (UserDetails)authentication.getPrincipal();
+        User user= userRepository.findByEmail(userDetails.getUsername()).get();
+        return userMapper.toDto(user);
 
     }
 
