d7f46cd4dabb8eb36af1becbf7f661309eef1686
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-12-09 01:44:00 +0100

    Add service test directory

---
+++ backend/src/test/java/com/backend/backend/service/impl/AuthServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.backend.backend.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthServiceImplTest {
+    @Test
+    void testLogin() {
+
+    }
+
+    @Test
+    void testLogout() {
+
+    }
+
+    @Test
+    void testMe() {
+
+    }
+
+    @Test
+    void testRefreshAccessToken() {
+
+    }
+
+    @Test
+    void testRegister() {
+
+    }
+}
