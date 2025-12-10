09a2ca7edf2f95eeb9d6e277d611c67350a6689a
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-12-10 21:08:24 +0100

    refactor: update login validation schema

--- frontend/src/utils/loginSchema.js
+++ frontend/src/utils/loginSchema.js
@@ -7,7 +7,7 @@ export const loginSchema = z.object({
     .min(6, "Password must at least 8 characters")
     .regex(/[A-Z]/, "Password must contain at lest one UpperCase")
     .regex(/[0-9]/, "Password must contain at least one number"),
-  rememberMe: z.boolean().refine((val) => val === true, {
-    message: "You must accept the terms condition",
+  acceptTerms: z.boolean().refine((val) => val === true, {
+    message: "You must Accept the terms conditions",
   }),
 });
