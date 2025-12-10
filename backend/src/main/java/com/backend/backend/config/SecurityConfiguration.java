ed6e2cca0eed924183882e3e64a09f8bd9df1426
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-12-10 21:08:19 +0100

    feat: update security configuration

--- backend/src/main/java/com/backend/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/backend/backend/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
