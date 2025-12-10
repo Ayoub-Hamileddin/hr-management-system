c427d8310d6ba3f5597e979158b4a59c05866595
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-12-10 21:08:21 +0100

    refactor: update UserMapper mapping logic

--- backend/src/main/java/com/backend/backend/mapper/UserMapper.java
+++ backend/src/main/java/com/backend/backend/mapper/UserMapper.java
@@ -1,16 +1,25 @@
 package com.backend.backend.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 import com.backend.backend.model.User;
-import com.backend.backend.payload.DTO.UserDto;
+import com.backend.backend.payload.DTO.UserDto.UserDto;
+import com.backend.backend.payload.DTO.UserDto.UserUpdateDto;
 
 @Mapper(componentModel = "spring")
+
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
 
     UserDto toDto(User user);
     User toEntity(UserDto userDto);
+
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE) 
+    void updateUserFromDto(UserUpdateDto dto,@MappingTarget User entity);
 }
