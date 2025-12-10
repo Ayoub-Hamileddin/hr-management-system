15d211a925c8b1bce26955b29127b2f56b747a7b
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-12-10 21:08:24 +0100

    feat: update frontend auth components

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -38,113 +38,113 @@ const Login = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
-            </h1>
-            <form
-              className="space-y-4 md:space-y-6"
-              onSubmit={handleSubmit(onSubmit)}
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0  ">
+        <div className="flex flex-col justify-center p-8 md:p-14">
+          <span className="mb-3 text-4xl font-bold">Welcome Back</span>
+          <span className="font-light text-gray-400 mb-8">
+            Welcome Back ! Please Enter Your Details
+          </span>
+          {/* form */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mt-4">
+              <span className="mb-2 text-base">Email</span>
+              <input
+                {...register("email")}
+                type="email"
+                name="email"
+                id="email"
+                className="w-full p-2 rounded-md border border-gray-300 placeholder:font-light placeholder:text-gray-500"
+                placeholder="name@company.com"
+                required=""
+              />
+            </div>
+            {errors.email && (
+              <span className="text-red-600 font-light text-xs">
+                {errors.email.message}
+              </span>
+            )}
+            <div className="mt-4">
+              <span className="mb-2 text-base">Password</span>
+              <input
+                {...register("password")}
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                required=""
+              />
+            </div>
+            {errors.password && (
+              <span className="text-red-600 font-light text-xs">
+                {errors.password.message}
+              </span>
+            )}
+            <div className="flex justify-between w-full pt-4">
+              <div className="mr-24">
+                <input
+                  {...register("acceptTerms")}
+                  id="terms"
+                  aria-describedby="terms"
+                  type="checkbox"
+                  required=""
+                  className="mr-2"
+                />
+                <span className="text-base">remember for 30 days </span>
+              </div>
+              <span className="font-bold text-base underline">
+                Forgot password{" "}
+              </span>
+            </div>
+            {errors.acceptTerms && (
+              <span className="text-red-600 font-light text-xs">
+                {errors.acceptTerms.message}
+              </span>
+            )}
+            <button
+              disabled={isLoading || isSubmitting}
+              className="w-full bg-black text-white text-md font-semibold p-2  mt-5 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300 "
             >
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
-                <input
-                  {...register("email")}
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
-                  required=""
-                />
-                {/* print error "email" */}
-                {errors.email && (
-                  <span className="text-red-600 font-medium text-xs">
-                    {errors.email.message}
-                  </span>
-                )}
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  {...register("password")}
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-                {/* print error (password) */}
-                {errors.rememberMe && (
-                  <span className="text-red-600 font-medium text-xs">
-                    {errors.rememberMe.message}
-                  </span>
-                )}
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      {...register("rememberMe")}
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label
-                      htmlFor="remember"
-                      className="text-gray-500 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-              </div>
-              {/* print error */}
-              {errors.password && (
-                <span className="text-red-600 font-medium text-xs">
-                  {errors.password.message}
-                </span>
-              )}
-              <button
-                disabled={isLoading || isSubmitting}
-                type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                {isSubmitting ? "Login ..." : "Sign in"}
-              </button>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
-                <Link
-                  to="/register"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Sign up
-                </Link>
-              </p>
-            </form>
+              {isSubmitting ? "Sign In ..." : "Sign In"}
+            </button>
+            <button className="w-full border border-gray-300 text-md font-semibold p-2 rounded-lg mb-6 hover:bg-black hover:text-white">
+              <img
+                src="src/assets/images/google.svg"
+                alt="google-svg"
+                className="w-6 h-6  inline mr-2"
+              />
+              Sign In with Google
+            </button>
+          </form>
+          {/* end form */}
+          <div className="text-center text-gray-400">
+            Dont'have an account ?
+            <span className="font-bold text-black underline">
+              {" "}
+              Sign up for free
+            </span>
+          </div>
+        </div>
+        <div className="relative">
+          <img
+            src="src/assets/images/hr-bg2.png"
+            alt=""
+            className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
+          />
+          {/* text on image */}
+          <div
+            className="absolute hidden bottom-10 right-6 p-6 bg-black bg-opacity-30 backdrop-blur-sm
+            rounded drop-shadow-lg md:block font-bold text-center
+          "
+          >
+            <span className="text-white text-xl">
+              Let' s manage your employees
+            </span>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
