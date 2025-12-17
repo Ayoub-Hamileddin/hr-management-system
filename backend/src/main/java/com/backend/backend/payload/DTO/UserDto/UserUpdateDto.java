534491ae089ff189e2d64083d37c57cc22229c5b
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-12-17 02:11:59 +0100

    modified: UserUpdateDto.java

--- backend/src/main/java/com/backend/backend/payload/DTO/UserDto/UserUpdateDto.java
+++ backend/src/main/java/com/backend/backend/payload/DTO/UserDto/UserUpdateDto.java
@@ -1,11 +1,6 @@
 package com.backend.backend.payload.DTO.UserDto;
 
-import java.time.LocalDateTime;
-
 import com.backend.backend.domain.Role;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 
