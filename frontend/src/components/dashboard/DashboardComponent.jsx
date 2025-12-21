77072d9c854250ce78c0ccc8af7c7e953e6b8db8
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-12-21 03:02:51 +0100

    feat: add new dashboard component directory and components

---
+++ frontend/src/components/dashboard/DashboardComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TopBar from "./TopBar";
+
+const DashboardComponent = () => {
+  return (
+    <div className="border-2  bg-white rounded-lg   pb-4 shadow-lg  h-[200vh]   ">
+      <TopBar />
+    </div>
+  );
+};
+
+export default DashboardComponent;
