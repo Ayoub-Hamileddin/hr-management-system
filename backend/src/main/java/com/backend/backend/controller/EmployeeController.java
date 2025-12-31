47428e4e7d571bb709d2a678a0e4aaa73d391f2a
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-01-01 00:26:41 +0100

    Update EmployeeController endpoints

--- backend/src/main/java/com/backend/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/backend/backend/controller/EmployeeController.java
@@ -1,12 +1,8 @@
 package com.backend.backend.controller;
 
 
-import com.backend.backend.payload.DTO.employeeDto.SetPasswordRequest;
 import com.backend.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +14,12 @@ public class EmployeeController {
     private final UserService userService;
 
 
-    @PostMapping("/set-password")
-    public ResponseEntity<String> activateEmployeeAccount(
-            @RequestBody  SetPasswordRequest setPasswordRequest
-    )
-    {
-        userService.setPassword(setPasswordRequest);
-       return ResponseEntity.ok("Password set successfully") ;
-    }
+//    @PostMapping("/set-password")
+//    public ResponseEntity<String> activateEmployeeAccount(
+//            @RequestBody  SetPasswordRequest setPasswordRequest
+//    )
+//    {
+//        userService.setPassword(setPasswordRequest);
+//       return ResponseEntity.ok("Password set successfully") ;
+//    }
 }
