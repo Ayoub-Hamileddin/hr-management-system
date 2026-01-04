c7f518034cd8fe6fef3266d4258ef68eec8c8574
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-01-04 02:29:50 +0100

    Modified AuthServiceImplTest

--- backend/src/test/java/com/backend/backend/service/impl/AuthServiceImplTest.java
+++ backend/src/test/java/com/backend/backend/service/impl/AuthServiceImplTest.java
@@ -1,30 +1,5 @@
 package com.backend.backend.service.impl;
 
-import org.junit.jupiter.api.Test;
-
 public class AuthServiceImplTest {
-    @Test
-    void testLogin() {
 
-    }
-
-    @Test
-    void testLogout() {
-
-    }
-
-    @Test
-    void testMe() {
-
-    }
-
-    @Test
-    void testRefreshAccessToken() {
-
-    }
-
-    @Test
-    void testRegister() {
-
-    }
 }
