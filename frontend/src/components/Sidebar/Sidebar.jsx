65c9483db5af9b8be5e1224bbbf75e6a7ca2b071
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-01-15 22:56:20 +0100

    modified: Sidebar.jsx

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import AccountToggle from "./AccountToggle";
-import Search from "./Search";
-import RouteSelect from "./RouteSelect";
-import Plan from "./Plan";
+import RouteSelect from "./SidebarItems";
+import Plan from "./SidebarFooter";
+import SidebarHeader from "./SidebarHeader";
+import SidebarSection from "./SidebarSection";
+import SidebarItems from "./SidebarItems";
+import SidebarFooter from "./SidebarFooter";
 
 const Sidebar = () => {
   return (
     <div className="">
-      <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-100px)]">
-        <AccountToggle />
-        <Search /> {/* TODO:  Cmdk package to implemnt cmd search    */}
-        <RouteSelect />
+      <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-100px-100px)]">
+        {/* Header */}
+        <div className="space-y-7 ">
+          <SidebarHeader />
+          <SidebarSection />
+        </div>
+
+        {/* links items */}
+        <div className="mt-[34px]">
+          <SidebarItems />
+        </div>
       </div>
-      <Plan />
+      {/* footer */}
+
+      <SidebarFooter />
     </div>
   );
 };
