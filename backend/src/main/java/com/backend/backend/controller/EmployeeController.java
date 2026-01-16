cfec87948f26e75bf6daa2bbc97430281429f72d
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-01-16 23:27:13 +0100

    modified: EmployeeController.java

--- backend/src/main/java/com/backend/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/backend/backend/controller/EmployeeController.java
@@ -1,14 +1,18 @@
 package com.backend.backend.controller;
 
 
+import com.backend.backend.payload.DTO.employeeDto.CreateEmployeeDto;
+import com.backend.backend.payload.DTO.employeeDto.EmployeeDto;
 import com.backend.backend.payload.DTO.employeeDto.SetPasswordRequest;
+import com.backend.backend.payload.DTO.employeeDto.UpdateEmployeeDto;
+import com.backend.backend.payload.response.DeleteResponse;
+import com.backend.backend.service.EmployeeService;
 import com.backend.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
@@ -16,14 +20,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     private final UserService userService;
-
+    private final EmployeeService employeeService;
 
     @PostMapping("/set-password")
-    public ResponseEntity<String> activateEmployeeAccount(
-            @RequestBody SetPasswordRequest setPasswordRequest
-    )
+    public ResponseEntity<String> activateEmployeeAccount(@RequestBody SetPasswordRequest setPasswordRequest)
     {
         userService.setPassword(setPasswordRequest);
        return ResponseEntity.ok("Password set successfully") ;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody CreateEmployeeDto createEmployeeDto)
+    {
+        return ResponseEntity.ok(employeeService.createEmployee(createEmployeeDto));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(
+            @RequestParam("id") Long employeeId,
+            @RequestBody UpdateEmployeeDto updateEmployeeDto)
+
+    {
+        return ResponseEntity.ok(employeeService.updateEmployee(employeeId,updateEmployeeDto));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DeleteResponse> deleteEmployee(@RequestParam("id") Long employeeId)
+    {
+        return  ResponseEntity.ok(employeeService.deleteEmployee(employeeId));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees()
+    {
+           return  ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@RequestParam("id") Long employeeId)
+    {
+           return  ResponseEntity.ok(employeeService.getEmployeeById(employeeId));
     }
 }
