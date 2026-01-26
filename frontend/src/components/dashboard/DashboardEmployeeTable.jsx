440b6b0e7a4739133b403d0c58e314f9a3c4f562
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-01-26 20:28:51 +0100

    Enhanced DashboardEmployeeTable component

--- frontend/src/components/dashboard/DashboardEmployeeTable.jsx
+++ frontend/src/components/dashboard/DashboardEmployeeTable.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TableFilter from "./TableFilter";
+import EmployeeTable from "../employees/EmployeeTable";
 
 const DashboardEmployeeTable = () => {
   return (
-    <div className="col-span-7  bg-white rounded-lg p-4 ">
+    <div className="col-span-8  bg-white rounded-lg p-4 ">
       <TableFilter />
+      <EmployeeTable />
     </div>
   );
 };
